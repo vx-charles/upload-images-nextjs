80afd068330bf1e0122df8ba884c71d7a71470b6
Charles Azevedo <vx.charles@gmail.com>
2023-03-04 00:31:02 -0300

    feat: upload do projeto completo.

---
+++ README.md
@@ -0,0 +1,44 @@
+<h1 align="center">upfi - Upload de imagens</h1>
+
+<img src=".github\upload-image-capa.jpg" alt="layout upfi" />
+
+# Sobre o projeto
+
+É aplicação onde o principal objetivo é fazer o upload de imagens e adicionar sua descrição usando React Hook Form. A aplicação foi estilizada com Chakra-UI. Ela realiza requisições para sua própria API Next.js que vai retornar os dados do FaunaDB (banco de dados) e do ImgBB (serviço de hospedagem de imagens).
+
+- Infinite Queries e Mutations com React Query;
+- Envio de formulário com React Hook Form;
+- Exibição de Modal e Toast com Chakra UI;
+- Entre outros.
+
+#Tecnologias usadas
+
+- Next.js
+- Chakra-ui
+- React Query;
+- React Hook Form;
+- ImgBB;
+- FaunaDB;
+- API do Next.js;
+- Axios;
+
+# Criação do arquivo .env
+
+Para o projeto executar sem erros na hora de trazer uma imagem, é necessário criar conta no [FaunaDB](https://dashboard.fauna.com) e no [ImgBB](https://api.imgbb.com) para que ocorra o armazenamendo de dados no FaunaDB e o armazenamento da image no ImgBB com essas variáveis dentro do arquivo __.env.local__:
+
+``` env
+NEXT_PUBLIC_IMGBB_API_KEY=
+FAUNA_API_KEY=
+```
+
+# Executando o projeto
+
+Execute o seguinte comando para iniciar a aplicação:
+
+`npm run dev`
+
+# Arquivos de test do projeto
+
+O projeto tem a pasta `__test__` que executa os testes com aprovações necessárias baseadas do curso e para ver basta executar este comando:
+
+`npm run test`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,27 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type Image = {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: number;
+};
+
+type GetImagesResponse = {
+  after: string;
+  data: Image[];
+};
+
 export default function Home(): JSX.Element {
+  const fetchImages = async ({ pageParam = 0 }): Promise<GetImagesResponse> => {
+    const response = await api.get('/api/images', {
+      params: { after: pageParam },
+    });
+    return response.data;
+  };
+
   const {
     data,
     isLoading,
@@ -17,19 +37,23 @@ export default function Home(): JSX.Element {
     fetchNextPage,
     hasNextPage,
   } = useInfiniteQuery(
-    'images',
     // TODO AXIOS REQUEST WITH PARAM
-    ,
+    'images',
+    fetchImages,
     // TODO GET AND RETURN NEXT PAGE PARAM
+    { getNextPageParam: lastPage => lastPage?.after || null }
   );
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages.flatMap(pageData => pageData.data.flat());
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
+  if (isLoading) return <Loading />;
 
   // TODO RENDER ERROR SCREEN
+  if (isError) return <Error />;
 
   return (
     <>
@@ -37,7 +61,14 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {
+          /* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */
+          hasNextPage && (
+            <Button onClick={() => fetchNextPage()} marginTop="2.5rem">
+              {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+            </Button>
+          )
+        }
       </Box>
     </>
   );
